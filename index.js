9210e673415e207400472ac5bed1ae4f4c70ed90
rich0021 <kurabersayap002@gmail.com>
2023-05-10 06:48:37 +0700

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+const myModule = require("./module.js");
+
+myModule.getNama();
+myModule.getTempatLahir();
+myModule.getTanggalLahir();
+myModule.getAlamat();
---
+++ module.js
@@ -0,0 +1,15 @@
+exports.getNama = function () {
+  console.log("Nama : Muhammad Naufal Muttaqin");
+};
+
+exports.getTempatLahir = function () {
+  console.log("Tempat Lahir : Bandung");
+};
+
+exports.getTanggalLahir = function () {
+  console.log("Tanggal Lahir : 23 November 2004");
+};
+
+exports.getAlamat = function () {
+  console.log("Alamat : Jl.Maleber Utara No.292");
+};
